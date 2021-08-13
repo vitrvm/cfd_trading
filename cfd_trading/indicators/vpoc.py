0a6b00917a4f10c8bee3fce2fa9ee3a08ba8bff5
Vitrvm <vitrvm@protonmail.com>
2021-08-13 20:55:24 +0200

    addede vpoc indicator

--- cfd_trading/indicators/vpoc.py
+++ cfd_trading/indicators/vpoc.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 
 def vpoc(df:pd.DataFrame, grouper='D'):
-    df_group = df.groupby(pd.Grouper(freq=grouper))
+    #df_group = df.groupby(pd.Grouper(freq=grouper))
 
     price_max = round(df['Bid'].max())
     price_min = round(df['Ask'].min())
     vol_profile = np.arange(101)
     vol_profile.fill(0.0)
 
-    for index, row in df_group.iterrows():
+    for index, row in df.iterrows():
         avg_price = round((row['Ask']+row['Bid'])/2)
 
         price = round(((avg_price-price_min)/(price_max-price_min))*100)
